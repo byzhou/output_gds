06e259c038b4d1e216b508011a31716177b96440
bob-icsg <bobzhou@bu.edu>
2015-02-23 17:50:38 -0500

    AUTOCOMMIT (bobzhou):Mon Feb 23 17:30:20 EST 2015

--- dumpGDS.py
+++ dumpGDS.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python
+
 import os
+
+#regular expression
+import re
+#python gds related
 import numpy
 import gdspy
 
+#choose the standard cell
 cellName    = 'AND2_X2'
 gdsii       = gdspy.GdsImport ( './gds/' + cellName + '.gds' )
 cellinfo    = gdsii.extract ( cellName )
@@ -10,9 +16,24 @@ cellinfo    = gdsii.extract ( cellName )
 #display the view. Btw, 9 10 11 layers are for the metals, vias and gates
 #gdspy.LayoutViewer()
 
+#get polygon info from the cell
 polyinfo    = cellinfo.get_polygons() 
 
+#write to the file of the polygon info
 writeFile   = open ( cellName + '.txt' , 'w' )
 print >> writeFile , polyinfo 
 writeFile.close()
 
+#print cell attributes values
+cellInst    = vars ( cellinfo )
+#search for word begin with 0x
+regux       = re.compile ( "0x\w+" )
+#tranform data type
+strInst     = str ( cellInst )
+#search for regux
+firstInst   = regux.match ( strInst )
+#print cellInst
+print firstInst
+
+#print polygon info
+#print  polyinfo
